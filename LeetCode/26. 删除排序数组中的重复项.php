ff024dfcee2b9a3bd8112e131b3dbd07ab2e543c
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-03 21:49:08 +0800

    增加LeetCode题目

---
+++ LeetCode/26. 删除排序数组中的重复项.php
@@ -0,0 +1,29 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums)
+    {
+        $v = $nums[0];
+        $len = 1;
+        $count = count($nums);
+        for ($j = 1; $j < $count; $j++) {
+            if ($nums[$j] == $v) {
+                unset($nums[$j]);
+            } else {
+                $v = $nums[$j];
+                $len++;
+            }
+        }
+        return $len;
+    }
+}
+
+$nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+$ret = (new Solution())->removeDuplicates($nums);
+$nums = [1, 1, 2];
+$ret = (new Solution())->removeDuplicates($nums);
