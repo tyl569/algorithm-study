61509ff32f62f6e28fd51e4fca8e499bff3ec9b3
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-05 18:36:28 +0800

    增加旋转数组

--- LeetCode/189. 旋转数组.php
+++ LeetCode/189. 旋转数组.php
@@ -27,9 +27,46 @@ class Solution
         }
         return $nums;
     }
+
+    /**
+     * @param $nums
+     * @param $k
+     * @return mixed
+     *
+     * 三次旋转
+     *
+     * 当我们旋转数组 k 次， k\%nk%n 个尾部元素会被移动到头部，剩下的元素会被向后移动。
+     *
+     * eg: [1,2,3,4,5,6,7]
+     *
+     * 1、我们先把所有数组反转：[7,6,5,4,3,2,1]
+     * 2、我们把前k个元素反正换：[5,6,7,4,3,2,1]
+     * 3、我们把k后面的元素反转：[5,6,7,1,2,3,4]
+     * 得到了目标数据
+     */
+    function rotateByReverse(&$nums, $k)
+    {
+        $k = $k % count($nums);
+        $this->Reverse($nums, 0, count($nums) - 1);
+        $this->Reverse($nums, 0, $k - 1);
+        $this->Reverse($nums, $k, count($nums) - 1);
+        return $nums;
+    }
+
+    function Reverse(&$nums, $start, $end)
+    {
+        while ($start < $end) {
+            $tmp = $nums[$end];
+            $nums[$end] = $nums[$start];
+            $nums[$start] = $tmp;
+            $start++;
+            $end--;
+        }
+    }
 }
 
 $nums = [1, 2, 3, 4, 5, 6, 7];
 $k = 3;
-$ret = (new Solution())->rotate($nums, $k);
+//$ret = (new Solution())->rotate($nums, $k);
+$ret = (new Solution())->rotateByReverse($nums, $k);
 var_dump($ret);
