253d7b026d380313bda234e91c0e155e4c47bfc8
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-07 16:03:47 +0800

    增加LeetCode题目

--- LeetCode/11. 盛最多水的容器.php
+++ LeetCode/11. 盛最多水的容器.php
@@ -19,6 +19,28 @@ class Solution
         }
         return $max;
     }
+
+    function maxArea2($height)
+    {
+        $max = 0;
+        $start = 0;
+        $end = count($height) - 1;
+        while ($start < $end) {
+
+            $l = $end - $start;
+            $h = min($height[$end], $height[$start]);
+            $max = max($max, $l * $h);
+
+            if ($height[$start] < $height[$end]) { // 谁更小，谁就挪动
+                $start++;
+            } else {
+                $end--;
+            }
+        }
+        return $max;
+    }
 }
 
 echo (new Solution())->maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+echo "\n";
+echo (new Solution())->maxArea2([1, 8, 6, 2, 5, 4, 8, 3, 7]);
