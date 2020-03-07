7ef19fbf590f392e98d3b83beb9fb8f2b624c5a9
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-07 16:12:52 +0800

    更改文件名

---
+++ LeetCode/70. 爬楼梯[简单].php
@@ -0,0 +1,21 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n) {
+        if ($n==1 || $n==2) {
+            return $n;
+        }
+        $num1 = 1;
+        $num2 = 2;
+        for($i= 3; $i<=$n; $i++) {
+            $tmp = $num2;
+            $num2 = $num2 + $num1;
+            $num1 = $tmp;
+        }
+        return $num2;
+    }
+}
