15230a5dd95951441ac8458b22b9a073934aaf72
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-08 15:44:09 +0800

    增加LeetCode题目

---
+++ LeetCode/24. 两两交换链表中的节点[中等].php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function swapPairs($head)
+    {
+        $pre = new ListNode(0);
+        $pre->next = $head;
+        $tmp = $pre;
+        while ($tmp->next != null && $tmp->next->next != null) {
+            $first = $tmp->next;
+            $second = $tmp->next->next;
+            $tmp->next = $second;
+            $first->next = $second->next;
+            $second->next = $first;
+            $tmp = $first;
+        }
+        return $pre->next;
+    }
+
+    function swapPairs2($head)
+    {
+        if ($head == null || $head->next == null) {
+            return $head;
+        }
+        $first = $head;
+        $second = $head->next;
+        $first->next = $this->swapPairs($second->next);
+        $second->next = $first;
+        return $second;
+    }
+}
+
+
+$node1 = new ListNode(1);
+$head->next = $node1;
+$node2 = new ListNode(2);
+$node1->next = $node2;
+$node3 = new ListNode(3);
+$node2->next = $node3;
+$node4 = new ListNode(4);
+$node3->next = $node4;
+
+function mock($head)
+{
+    $head1 = $head;
+    echo "调换前链表:";
+    while ($head != null) {
+        echo $head->val;
+        $head = $head->next;
+    }
+    echo "\n";
+    $newHead = (new Solution())->swapPairs($head1);
+    echo "调换后链表:";
+    while ($newHead != null) {
+        echo $newHead->val;
+        $newHead = $newHead->next;
+    }
+}
+
+mock($node1);
