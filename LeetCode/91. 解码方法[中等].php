1cbe48f35397bde3644a122aaa3470e29d1c9f71
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-13 08:15:48 +0800

    修改代码

--- LeetCode/91. 解码方法[中等].php
+++ LeetCode/91. 解码方法[中等].php
@@ -34,9 +34,17 @@ class Solution
     function numDecodings2($s)
     {
         $s = strval($s);
-        $dp[strlen($s)] = 1;
+        // 最后一位如果0，那么就是0种
         $dp[strlen($s) - 1] = $s{strlen($s) - 1} == 0 ? 0 : 1;
-        for ($i = strlen($s) - 2; $i >= 0; $i--) {
+        // 如果倒数第2位是0，那么0种
+        // 如果后两位的和<27，额外+1
+        if ($s{strlen($s) - 2} == 0) {
+            $dp[strlen($s) - 2] = 0;
+        } else {
+            $dp[strlen($s) - 2] = $dp[strlen($s) - 1] + (($s{strlen($s) - 2} * 10 + $s{strlen($s) - 1}) < 27 ? 1 : 0);
+        }
+
+        for ($i = strlen($s) - 3; $i >= 0; $i--) {
             if ($s{$i} == 0) {
                 $dp[$i] = 0;
             } else {
@@ -47,6 +55,6 @@ class Solution
     }
 }
 
-echo (new Solution())->numDecodings('1223');
+echo (new Solution())->numDecodings('1223104');
 echo "\n";
-echo (new Solution())->numDecodings2('1223');
+echo (new Solution())->numDecodings2('1223104');
