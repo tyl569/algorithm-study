257a09fc34a05608eb6eea5e751d658e7a8e9821
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-14 20:20:26 +0800

    增加LeetCode题目

--- LeetCode/129. 求根到叶子节点数字之和[中等].php
+++ LeetCode/129. 求根到叶子节点数字之和[中等].php
@@ -1,7 +1,84 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: tengyunlong
- * Date: 2020-03-14
- * Time: 19:36
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
  */
+
+class TreeNode
+{
+    public $val = null;
+    public $left = null;
+    public $right = null;
+
+    function __construct($value)
+    {
+        $this->val = $value;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function sumNumbers($root)
+    {
+        $this->dfs($root, 0);
+        return $this->sum;
+    }
+
+    private $sum = 0;
+
+    function dfs($node, $rootVal)
+    {
+        if ($node == null) {
+            return;
+        }
+        $current = 10 * $rootVal + $node->val;
+        if ($node->left == null && $node->right == null) {
+            $this->sum += $current;
+            return;
+        }
+        $this->dfs($node->left, $current);
+        $this->dfs($node->right, $current);
+    }
+}
+
+mock();
+
+function mock()
+{
+    $root = new TreeNode(1);
+
+    $left1 = new TreeNode(2);
+    $right1 = new TreeNode(3);
+
+    $left11 = new TreeNode(4);
+
+    $root->left = $left1;
+    $root->right = $right1;
+    $left1->left = $left11;
+
+
+    /**
+     *      1
+     *     / \
+     *    2   3
+     *   /
+     *  4
+     *
+     * ret: 124 + 13 = 137
+     */
+
+    echo "======= test case start =======\n";
+    echo (new Solution())->sumNumbers($root)."\n";
+    echo "======= test case end =========\n";
+}
