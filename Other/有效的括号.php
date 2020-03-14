cb6776506d2a209b42f460fb530c6c3effcff10d
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-14 15:32:05 +0800

    修改代码

--- Other/有效的括号.php
+++ Other/有效的括号.php
@@ -12,31 +12,28 @@
 
 function solution_1($s)
 {
-    $len = strlen($s);
-    $num = 0;
-    for ($i = 0; $i < $len; $i++) {
+    $count = 0;
+    for ($i = 0; $i < strlen($s); $i++) {
         if ($s{$i} == "(") {
-            $num++;
+            $count++;
+        } else {
+            $count--;
         }
-        if ($s{$i} == ")") {
-            $num--;
-        }
-        if ($num < 0) {
+        if ($count < 0) {
             return false;
         }
-
     }
-    return $num == 0;
+    return $count == 0;
 }
 
 function solution_2($s)
 {
-    $count = $need = 0;
+    $count = 0;
+    $need = 0;
     for ($i = 0; $i < strlen($s); $i++) {
         if ($s{$i} == "(") {
             $count++;
-        }
-        if ($s{$i} == ")") {
+        } else {
             $count--;
         }
         if ($count == -1) {
@@ -49,25 +46,21 @@ function solution_2($s)
 
 function solution_3($s)
 {
-    if (empty($s)) {
-        return 0;
-    }
+    $max = 0;
     $dp = [];
-    $res = 0;
-    // 思路：统计以dp[i]结尾的最长子串的长度
-    for ($i = 1; $i < strlen($s); $i++) {
+    for ($i = 0; $i < strlen($s); $i++) {
         $dp[$i] = 0;
         if ($s{$i} == ")") {
-            $pre = $i - 1 - $dp[$i - 1];
+            $pre = $i - 1 - (isset($dp[$i - 1]) ? $dp[$i - 1] : 0);
             if ($pre >= 0 && $s{$pre} == "(") {
-                $dp[$i] = $dp[$i - 1] + 2 + ($pre > 0 ? $dp[$pre - 1] : 0);
+                $dp[$i] = $dp[$i - 1] + 2 + (isset($dp[$pre - 1]) ? $dp[$pre - 1] : 0);
             }
         }
-        $res = max($res, $dp[$i]);
-
+        $max = max($max, $dp[$i]);
     }
-    return $res;
+    return $max;
 }
+
 
 function mock()
 {
@@ -89,7 +82,7 @@ function mock()
     $ret1 = solution_3("()");
     $ret2 = solution_3("(())");
     $ret3 = solution_3(")()()(");
-    $ret4 = solution_3(")))((((()()())()");
+    $ret4 = solution_3("((())(())");
     var_dump($ret1, $ret2, $ret3, $ret4);
     echo "=======test case for quest 3 end ====== \n";
 }
