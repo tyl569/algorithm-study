fdbc92ac6decacff9ce7241968b182d01f8e5101
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-14 17:01:59 +0800

    增加解法

--- LeetCode/32. 最长有效括号[困难].php
+++ LeetCode/32. 最长有效括号[困难].php
@@ -23,6 +23,42 @@ class Solution
         }
         return $max;
     }
+
+    function longestValidParentheses2($s)
+    {
+        $left = 0;
+        $right = 0;
+        $max = 0;
+        for ($i = 0; $i < strlen($s); $i++) {
+            if ($s{$i} == "(") {
+                $left++;
+            } else {
+                $right++;
+            }
+            if ($left == $right) {
+                $max = max($max, $left * 2);
+            }
+            if ($right > $left) {
+                $left == $right = 0;
+            }
+        }
+        $left = $right = 0;
+        for ($i = strlen($s) - 1; $i >= 0; $i--) {
+            if ($s{$i} == "(") {
+                $left++;
+            } else {
+                $right++;
+            }
+            if ($left == $right) {
+                $max = max($max, $right * 2);
+            }
+            if ($left > $right) {
+                $left = $right = 0;
+            }
+
+        }
+        return $max;
+    }
 }
 
 mock();
@@ -34,6 +70,12 @@ function mock()
     echo (new Solution())->longestValidParentheses("(()()()()()");
     echo "\n";
     echo (new Solution())->longestValidParentheses("((())()()()()");
+    echo "\n";
+    echo (new Solution())->longestValidParentheses2("()()()()()");
+    echo "\n";
+    echo (new Solution())->longestValidParentheses2("(()()()()()");
+    echo "\n";
+    echo (new Solution())->longestValidParentheses2("((())()()()()");
     echo "\n";
     echo "======== test case end =========";
 }
