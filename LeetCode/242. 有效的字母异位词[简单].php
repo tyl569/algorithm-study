46a08678986c8da5040857512e378716d7aa8755
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-21 22:11:30 +0800

    增加解法

--- LeetCode/242. 有效的字母异位词[简单].php
+++ LeetCode/242. 有效的字母异位词[简单].php
@@ -31,6 +31,22 @@ class Solution
         }
         return true;
     }
+
+    // 字符太长容易计算出现溢出
+    function isAnagram_1($s, $t)
+    {
+        if (strlen($s) != strlen($t)) {
+            return false;
+        }
+        $prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103];
+        $sLen = 1;
+        $tLen = 1;
+        for ($i = 0; $i < strlen($s); $i++) {
+            $sLen *= $prime[ord($s{$i}) - 97];
+            $tLen *= $prime[ord($t{$i}) - 97];
+        }
+        return $sLen == $tLen;
+    }
 }
 
 
@@ -41,5 +57,8 @@ function mock()
     echo "======= test case start =======\n";
     var_dump((new Solution())->isAnagram("anagram", "nagaram"));
     var_dump((new Solution())->isAnagram("rat", "car"));
+    var_dump((new Solution())->isAnagram_1("anagram", "nagaram"));
+    var_dump((new Solution())->isAnagram_1("rat", "car"));
+    var_dump((new Solution())->isAnagram_1("a", "ab"));
     echo "======= test case end =======\n";
 }
