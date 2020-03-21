5acfa14c9e03a1b81e1a5c10b3fdbd03c7e8df21
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-21 12:39:10 +0800

    增加矩阵的解法

--- LeetCode/70. 爬楼梯[简单].php
+++ LeetCode/70. 爬楼梯[简单].php
@@ -21,4 +21,51 @@ class Solution
         }
         return $num2;
     }
+
+    function climbStairs_2($n)
+    {
+        $base = [
+            [1, 1],
+            [1, 0]
+        ];
+        $ans = [
+            [1, 0],
+            [0, 1]
+        ];
+        while ($n) {
+            if ($n & 1) {
+                $ans = $this->multi($ans, $base);
+            }
+            $base = $this->multi($base, $base);
+            $n >>= 1;
+        }
+        return $ans[0][0];
+    }
+
+    function multi($a, $b)
+    {
+        $c = [
+            [0, 0],
+            [0, 0]
+        ];
+        for ($i = 0; $i < 2; $i++) {
+            for ($j = 0; $j < 2; $j++) {
+                for ($k = 0; $k < 2; $k++) {
+                    $c[$i][$j] = $c[$i][$j] + $a[$i][$k] * $b[$k][$j];
+                }
+            }
+        }
+        return $c;
+    }
+}
+
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    echo (new Solution())->climbStairs(3) . "\n";
+    echo (new Solution())->climbStairs_2(3) . "\n";
+    echo "======= test case end =======\n";
 }
