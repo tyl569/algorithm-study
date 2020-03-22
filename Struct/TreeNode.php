6f722677632e60b1fbef95fd35b4af99141394f7
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-22 10:31:28 +0800

    增加二叉树节点数据结构

---
+++ Struct/TreeNode.php
@@ -0,0 +1,13 @@
+<?php
+
+class TreeNode
+{
+    public $val = null;
+    public $left = null;
+    public $right = null;
+
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
