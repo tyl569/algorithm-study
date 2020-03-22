c604197cbd987f035b18dd2f7a15f0c67275e731
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-22 10:28:13 +0800

    增加LeetCode题目

---
+++ LeetCode/94. 二叉树的中序遍历[中等].php
@@ -0,0 +1,64 @@
+<?php
+
+class TreeNode
+{
+    public $val = null;
+    public $left = null;
+    public $right = null;
+
+    function __construct($value)
+    {
+        $this->val = $value;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function inorderTraversal($root)
+    {
+        if ($root == null) {
+            return [];
+        }
+        $res = [];
+        $this->helper($root, $res);
+        return $res;
+    }
+
+    function helper($node, &$res)
+    {
+        if ($node != null) {
+            if ($node->left != null) {
+                $this->helper($node->left, $res);
+            }
+            $res[] = $node->val;
+            if ($node->right != null) {
+                $this->helper($node->right, $res);
+            }
+        }
+        return $res;
+    }
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    $root = new TreeNode(1);
+    $node1 = new TreeNode(null);
+    $node2 = new TreeNode(2);
+    $node3 = new TreeNode(3);
+    $root->left = $node1;
+    $root->right = $node2;
+    $node2->left = $node3;
+
+    $ret = (new Solution())->inorderTraversal($root);
+    var_dump($ret);
+
+    echo "======= test case end\n";
+}
