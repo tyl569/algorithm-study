7c4cb9715887871f8037e3dca46c025a9f355ea6
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-22 11:44:32 +0800

    增加LeetCode题目

---
+++ LeetCode/144. 二叉树的前序遍历[中等].php
@@ -0,0 +1,53 @@
+<?php
+require_once "../Struct/TreeNode.php";
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function preorderTraversal($root)
+    {
+        if ($root == null) {
+            return [];
+        }
+        $res = [];
+        $this->helper($root, $res);
+        return $res;
+    }
+
+    function helper($node, &$res)
+    {
+        if ($node != null) {
+            $res[] = $node->val;
+            if ($node->left != null) {
+                $this->helper($node->left, $res);
+            }
+            if ($node->right != null) {
+                $this->helper($node->right, $res);
+            }
+        }
+        return $res;
+    }
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    $root = new TreeNode(1);
+    $node1 = null;
+    $node2 = new TreeNode(2);
+    $node3 = new TreeNode(3);
+    $root->left = $node1;
+    $root->right = $node2;
+    $node2->left = $node3;
+
+    $ret = (new Solution())->preorderTraversal($root);
+    var_dump($ret);
+
+    echo "======= test case end\n";
+}
