a484dd354946f5e3679c8ea1087cf1a293312452
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-23 20:38:56 +0800

    增加打印函数

--- Struct/TreeNode.php
+++ Struct/TreeNode.php
@@ -10,4 +10,20 @@ class TreeNode
     {
         $this->val = $val;
     }
+
+    static function printTree($node)
+    {
+        if ($node == null) {
+            return;
+        }
+        if ($node->left) {
+            echo "节点: ".$node->val." 的左节点为: ".$node->left->val."\n";
+        }
+        if ($node->right) {
+            echo "节点: ".$node->val." 的右节点为: ".$node->right->val."\n";
+        }
+        self::printTree($node->left);
+        self::printTree($node->right);
+    }
 }
+
