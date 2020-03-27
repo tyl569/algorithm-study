7fd9499c4c1ff924aa931c603e17da10e8262d49
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-27 20:50:47 +0800

    增加case

--- LeetCode/104. 二叉树的最大深度[简单].php
+++ LeetCode/104. 二叉树的最大深度[简单].php
@@ -16,6 +16,31 @@ class Solution
         }
         $left = $this->maxDepth($root->left);
         $right = $this->maxDepth($root->right);
-        return max($left, $right) + 1;
+        return max($left, $right)+1;
     }
 }
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    $root = new TreeNode(3);
+
+    $node2 = new TreeNode(9);
+    $node3 = new TreeNode(20);
+
+    $node4 = new TreeNode(15);
+    $node5 = new TreeNode(17);
+
+    $root->left = $node2;
+    $root->right = $node3;
+    $node3->left = $node4;
+    $node3->right = $node5;
+
+    echo (new Solution())->maxDepth($root)."\n";
+
+    echo "======= test case end =======\n";
+
+}
+
