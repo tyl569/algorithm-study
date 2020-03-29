d9e559f68894293a94128d3c4fba26d21a7e29d9
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-03-29 10:28:05 +0800

    增加LeetCode题目

---
+++ LeetCode/46. 全排列[中等].php
@@ -0,0 +1,44 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    private $result = [];
+
+    function permute($nums)
+    {
+        $this->helper($nums, 0, []);
+        return $this->result;
+    }
+
+    function helper($nums, $start, $list)
+    {
+        if (count($list) == count($nums)) {
+            $this->result[] = $list;
+            return;
+        }
+
+        for ($i = 0; $i < count($nums); $i++) {
+            if (in_array($nums[$i], $list)) {
+                continue;
+            }
+            $list[] = $nums[$i];
+            $this->helper($nums, $start + 1, $list);
+            array_pop($list);
+        }
+    }
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    $nums = [1, 2, 3];
+    var_dump((new Solution())->permute($nums));
+    echo "======= test case end =======\n";
+}
