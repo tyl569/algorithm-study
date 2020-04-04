e34372494c0d536d5b527cb2c4a70cdd1cb0cfdb
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-04 22:57:39 +0800

    增加广度优先算法

--- LeetCode/200. 岛屿数量[中等].php
+++ LeetCode/200. 岛屿数量[中等].php
@@ -34,6 +34,46 @@ class Solution
         $this->dfs($grid, $r, $c - 1);
         $this->dfs($grid, $r, $c + 1);
     }
+
+    function numIslands_2($grid)
+    {
+        $count = 0;
+
+        for ($i = 0; $i < count($grid); $i++) {
+            for ($j = 0; $j < count($grid[0]); $j++) {
+                if ($grid[$i][$j] == 1) {
+                    $count++;
+                    $grid[$i][$j] = 0;
+                    $queue = new SplQueue();
+                    $queue->push([$i, $j]);
+                    while (!$queue->isEmpty()) {
+                        $pop = $queue->dequeue();
+                        $x = $pop[0];
+                        $y = $pop[1];
+                        if ($x + 1 < count($grid) && $grid[$x + 1][$y] == 1) {
+                            $queue->push([$x + 1, $y]);
+                            $grid[$x + 1][$y] = 0;
+                        }
+                        if ($x - 1 >= 0 && $grid[$x - 1][$y] == 1) {
+                            $queue->push([$x - 1, $y]);
+                            $grid[$x - 1][$y] = 0;
+                        }
+                        if ($y - 1 >= 0 && $grid[$x][$y - 1] == 1) {
+                            $queue->push([$x, $y - 1]);
+                            $grid[$x][$y - 1] = 0;
+                        }
+                        if ($y + 1 < count($grid[0]) && $grid[$x][$y + 1] == 1) {
+                            $queue->push([$x, $y + 1]);
+                            $grid[$x][$y + 1] = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+        return $count;
+
+    }
 }
 
 mock();
@@ -48,5 +88,6 @@ function mock()
         [1, 0, 1]
     ];
     echo (new Solution())->numIslands($grid) . "\n";
+    echo (new Solution())->numIslands_2($grid) . "\n";
     echo "======= test case end =======\n";
 }
