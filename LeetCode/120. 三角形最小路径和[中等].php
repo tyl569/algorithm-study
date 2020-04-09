7571e65fbe51862c462b0d253285b5ee4f7d3013
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-09 23:59:10 +0800

    增加分治的解法

--- LeetCode/120. 三角形最小路径和[中等].php
+++ LeetCode/120. 三角形最小路径和[中等].php
@@ -16,6 +16,24 @@ class Solution
         }
         return $triangle[0][0];
     }
+
+    //分治
+    function minimumTotal_2($triangle)
+    {
+        return $this->helper(0, 0, $triangle);
+    }
+
+    function helper($i, $j, $triangle)
+    {
+        if ($i == count($triangle) - 1) {
+            return $triangle[$i][$j];
+        }
+
+        // 解决子问题
+        $left = $this->helper($i + 1, $j, $triangle);
+        $right = $this->helper($i + 1, $j + 1, $triangle);
+        return min($left, $right) + $triangle[$i][$j];
+    }
 }
 
 mock();
@@ -31,6 +49,7 @@ function mock()
         [4, 1, 8, 3],
     ];
     echo (new Solution())->minimumTotal($triangle) . "\n";
+    echo (new Solution())->minimumTotal_2($triangle) . "\n";
 
     $triangle = [
         [1],
@@ -39,6 +58,7 @@ function mock()
         [1, 0, 0, 1],
     ];
     echo (new Solution())->minimumTotal($triangle) . "\n";
+    echo (new Solution())->minimumTotal_2($triangle) . "\n";
 
     $triangle = [
         [-1],
@@ -46,6 +66,7 @@ function mock()
         [1, -1, -3]
     ];
     echo (new Solution())->minimumTotal($triangle) . "\n";
+    echo (new Solution())->minimumTotal_2($triangle) . "\n";
 
     echo "======= test case end =======\n";
 }
