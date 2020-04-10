0a7d29800fa31dd17f76006e5a4c7155b664b44f
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-10 21:23:33 +0800

    修改代码

--- LeetCode/53. 最大子序和[简单].php
+++ LeetCode/53. 最大子序和[简单].php
@@ -10,11 +10,21 @@ class Solution
     function maxSubArray($nums)
     {
         $max = $nums[0];
-        for ($i = 0; $i < count($nums); $i++) {
+        for ($i = 1; $i < count($nums); $i++) {
             if ($nums[$i - 1] > 0) {
                 $nums[$i] += $nums[$i - 1];
             }
-            $max = max($nums[$i], $max);
+            $max = max($max, $nums[$i]);
+        }
+        return $max;
+    }
+
+    function maxSubArray_2($nums)
+    {
+        $cur = $max = $nums[0];
+        for ($i = 1; $i < count($nums); $i++) {
+            $cur = max($cur + $nums[$i], $nums[$i]);
+            $max = max($max, $cur);
         }
         return $max;
     }
@@ -27,5 +37,7 @@ function mock()
     echo "======= test case start =======\n";
     echo (new Solution())->maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) . "\n";
     echo (new Solution())->maxSubArray([-2, 1]) . "\n";
+    echo (new Solution())->maxSubArray_2([-2, 1, -3, 4, -1, 2, 1, -5, 4]) . "\n";
+    echo (new Solution())->maxSubArray_2([-2, 1]) . "\n";
     echo "======= test case end =======\n";
 }
