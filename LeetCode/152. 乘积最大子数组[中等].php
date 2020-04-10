fc5d2f6f9891fff9ba613436c20c140a9db962e6
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-10 21:41:01 +0800

    增加LeetCode题目

---
+++ LeetCode/152. 乘积最大子数组[中等].php
@@ -0,0 +1,37 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function maxProduct($nums)
+    {
+        $imax = 1;
+        $imin = 1;
+        $max = PHP_INT_MIN;
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($nums[$i] < 0) {
+                $tmp = $imax;
+                $imax = $imin;
+                $imin = $tmp;
+            }
+            $imax = max($nums[$i], $imax * $nums[$i]);
+            $imin = min($nums[$i], $imin * $nums[$i]);
+            $max = max($max, $imax);
+        }
+        return $max;
+    }
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    echo (new Solution())->maxProduct([2, 3, -2, 4]) . "\n";
+    echo (new Solution())->maxProduct([2, 10, -2, -1]) . "\n";
+    echo "======= test case end =======\n";
+}
