c8960f9f6dc5533ad4567f77114f0da51a2d7db3
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-11 13:25:38 +0800

    修改代码

--- LeetCode/122. 买卖股票的最佳时机 II[简单].php
+++ LeetCode/122. 买卖股票的最佳时机 II[简单].php
@@ -17,6 +17,19 @@ class Solution
         }
         return $earn;
     }
+
+    function maxProfit_2($prices)
+    {
+        $db_i_0 = 0;
+        $db_i_1 = PHP_INT_MIN;
+
+        for ($i = 0; $i < count($prices); $i++) {
+            $db_i_0 = max($db_i_0, $db_i_1+$prices[$i]);
+            $db_i_1 = max($db_i_1, $db_i_0-$prices[$i]);
+
+        }
+        return $db_i_0;
+    }
 }
 
 mock();
@@ -27,6 +40,10 @@ function mock()
     echo (new Solution())->maxProfit([7, 1, 5, 3, 6, 4]) . "\n";
     echo (new Solution())->maxProfit([1, 2, 3, 4, 5]) . "\n";
     echo (new Solution())->maxProfit([7, 6, 5, 4, 3, 2, 1]) . "\n";
+
+    echo (new Solution())->maxProfit_2([7, 1, 5, 3, 6, 4]) . "\n";
+    echo (new Solution())->maxProfit_2([1, 2, 3, 4, 5]) . "\n";
+    echo (new Solution())->maxProfit_2([7, 6, 5, 4, 3, 2, 1]) . "\n";
 
     echo "======= test case end =======\n";
 }
