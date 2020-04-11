3022afbcf1a819a01a7d7fd380dba27cdcd4fa17
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-11 21:01:35 +0800

    修改代码

--- LeetCode/121. 买卖股票的最佳时机[简单].php
+++ LeetCode/121. 买卖股票的最佳时机[简单].php
@@ -16,9 +16,27 @@ class Solution
         $dp_i_1 = PHP_INT_MIN;
         for ($i = 0; $i < count($prices); $i++) {
             $dp_i_0 = max($dp_i_0, $dp_i_1 + $prices[$i]);
-            $dp_i_1 = max($dp_i_1, - $prices[$i]);
+            $dp_i_1 = max($dp_i_1, -$prices[$i]);
         }
         return $dp_i_0;
+    }
+
+    function maxProfit_2($prices)
+    {
+        if (empty($prices)) {
+            return 0;
+        }
+        $dp = [];
+        for ($i = 0; $i < count($prices); $i++) {
+            if ($i == 0) {
+                $dp[0][0] = 0;
+                $dp[0][1] = -$prices[0];
+                continue;
+            }
+            $dp[$i][0] = max($dp[$i - 1][0], $dp[$i - 1][1] + $prices[$i]);
+            $dp[$i][1] = max($dp[$i - 1][1],  - $prices[$i]);
+        }
+        return $dp[count($prices) - 1][0];
     }
 }
 
@@ -29,5 +47,7 @@ function mock()
     echo "======= test case start =======\n";
     echo (new Solution())->maxProfit([7, 1, 5, 3, 6, 4]) . "\n";
     echo (new Solution())->maxProfit([7, 6, 4, 3, 1]) . "\n";
+    echo (new Solution())->maxProfit_2([7, 1, 5, 3, 6, 4]) . "\n";
+    echo (new Solution())->maxProfit_2([7, 6, 4, 3, 1]) . "\n";
     echo "======= test case end =======\n";
 }
