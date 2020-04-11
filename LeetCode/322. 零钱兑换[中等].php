accc3b553dfb33aec42d418744a18c27e6041b8f
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-11 21:29:23 +0800

    增加LeetCode题目

---
+++ LeetCode/322. 零钱兑换[中等].php
@@ -0,0 +1,35 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $coins
+     * @param Integer $amount
+     * @return Integer
+     */
+    function coinChange($coins, $amount)
+    {
+        $dp[0] = 0;
+        for ($i = 1; $i <= $amount; $i++) {
+            $dp[$i] = $amount + 1;
+        }
+        for ($i = 1; $i <= $amount; $i++) {
+            for ($j = 0; $j < count($coins); $j++) {
+                if ($coins[$j] <= $i) {
+                    $dp[$i] = min($dp[$i], $dp[$i - $coins[$j]] + 1);
+                }
+            }
+        }
+        return $dp[$amount] > $amount ? -1 : $dp[$amount];
+    }
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    echo (new Solution())->coinChange([1, 2, 5], 11) . "\n";
+    echo "======= test case end =======\n";
+}
