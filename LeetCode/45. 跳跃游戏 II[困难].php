0b5dfb219de855c19e7e7c223c21a20148347ecf
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-13 18:11:15 +0800

    修改代码

--- LeetCode/45. 跳跃游戏 II[困难].php
+++ LeetCode/45. 跳跃游戏 II[困难].php
@@ -7,45 +7,45 @@ class Solution
      * @param Integer[] $nums
      * @return Integer
      */
-    private $result = [];
-
     // 让开始起跳的位置尽量大
     function jump($nums)
     {
-        $start = $ans = 0;
+        $start = 0;
         $end = 1;
-        while ($end < count($nums)) {
-
-            $maxPos = 0;
+        $ans = 0;
+        while ($end <= count($nums) - 1) {
+            $maxPost = 0;
             for ($i = $start; $i < $end; $i++) {
-                $maxPos = max($maxPos, $nums[$i] + $i);
+                $maxPost = max($maxPost, $i + $nums[$i]);
             }
-            $start = $end;
-            $end = $maxPos + 1;
-            $ans++;
 
+            $start = $end;
+            $end = $maxPost + 1;
+            $ans++;
         }
         return $ans;
     }
 
+    private $step;
 
     function jump_2($nums)
     {
-        $this->helper($nums, 0, []);
-        return min($this->result);
+        $this->step = count($nums) - 1;
+        $this->helper($nums, 0, 0);
+        return $this->step;
     }
 
-    function helper($nums, $pos, $list)
+    function helper($nums, $pos, $step)
     {
-        if ($pos == count($nums) - 1) {
-            $this->result[] = count($list);
+        if ($pos >= count($nums) - 1) {
+            $this->step = min($this->step, $step);
             return;
         }
-        $furthest = min($pos + $nums[$pos], count($nums) - 1);
-        for ($nextPost = $pos + 1; $nextPost <= $furthest; $nextPost++) {
-            $list[] = $pos;
-            $this->helper($nums, $nextPost, $list);
-            array_pop($list);
+        $further = min($pos + $nums[$pos], count($nums) - 1);
+        for ($i = $pos + 1; $i <= $further; $i++) {
+            $step++;
+            $this->helper($nums, $i, $step);
+            $step--;
         }
     }
 }
@@ -57,6 +57,5 @@ function mock()
     echo "======= test case start =======\n";
     var_dump((new Solution())->jump([2, 3, 1, 1, 4]));
     var_dump((new Solution())->jump_2([2, 3, 1, 1, 4]));
-
     echo "======= test case end =======\n";
 }
