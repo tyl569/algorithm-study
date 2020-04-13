ae42314f4c67f94ec3cf7e226a0f3c1f0669b84e
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-13 23:37:34 +0800

    修改代码

--- LeetCode/91. 解码方法[中等].php
+++ LeetCode/91. 解码方法[中等].php
@@ -15,17 +15,18 @@ class Solution
 
     function times($s, $index)
     {
-        if (strlen($s) == $index) {
+        if ($index == strlen($s)) {
             return 1;
         }
         if ($s{$index} == 0) {
             return 0;
         }
+
         $res = $this->times($s, $index + 1);
         if ($index == strlen($s) - 1) {
             return $res;
         }
-        if (($s{$index} * 10 + $s{$index + 1}) < 27) {
+        if (10 * $s{$index} + $s{$index + 1} <= 26) {
             $res += $this->times($s, $index + 2);
         }
         return $res;
@@ -33,23 +34,24 @@ class Solution
 
     function numDecodings2($s)
     {
-        $s = strval($s);
-        // 最后一位如果0，那么就是0种
-        $dp[strlen($s) - 1] = $s{strlen($s) - 1} == 0 ? 0 : 1;
-        // 如果倒数第2位是0，那么0种
-        // 如果后两位的和<27，额外+1
-        if ($s{strlen($s) - 2} == 0) {
-            $dp[strlen($s) - 2] = 0;
+        $len = strlen($s);
+        if ($s{$len - 1} == 0) {
+            $dp[$len - 1] = 0;
         } else {
-            $dp[strlen($s) - 2] = $dp[strlen($s) - 1] + (($s{strlen($s) - 2} * 10 + $s{strlen($s) - 1}) < 27 ? 1 : 0);
+            $dp[$len - 1] = 1;
         }
-
-        for ($i = strlen($s) - 3; $i >= 0; $i--) {
+        if ($s{$len - 2} == 0) {
+            $dp[$len - 2] = 0;
+        } else {
+            $dp[$len - 2] = $dp[$len - 1] + (($s{$len - 2} * 10 + $s{$len - 1}) < 27 ? 1 : 0);
+        }
+        for ($i = $len - 3; $i >= 0; $i--) {
             if ($s{$i} == 0) {
                 $dp[$i] = 0;
             } else {
                 $dp[$i] = $dp[$i + 1] + ((($s{$i} * 10 + $s{$i + 1}) < 27) ? $dp[$i + 2] : 0);
             }
+
         }
         return $dp[0];
     }
