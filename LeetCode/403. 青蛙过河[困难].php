ba9eb2f05febc92a7a477d90b0f93c66718e24c0
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-15 17:58:35 +0800

    修改代码

--- LeetCode/403. 青蛙过河[困难].php
+++ LeetCode/403. 青蛙过河[困难].php
@@ -24,6 +24,34 @@ class Solution
         }
         return $cur == count($stones) - 1;
     }
+
+    private $memo = [];
+
+    function canCross_2($stones)
+    {
+        for ($i = 0; $i <= count($stones); $i++) {
+            $this->memo[] = array_fill(0, count($stones), -1);
+        }
+        return $this->helper_2($stones, 0, 0) == 1;
+    }
+
+    function helper_2($stones, $k, $cur)
+    {
+        if ($this->memo[$cur][$k] >= 0) {
+            return $this->memo[$cur][$k];
+        }
+        for ($i = $cur + 1; $i < count($stones); $i++) {
+            $gap = $stones[$i] - $stones[$cur];
+            if ($gap >= $k - 1 && $gap <= $k + 1) {
+                if ($this->helper($stones, $gap, $i)) {
+                    $this->memo[$cur][$gap] = 1;
+                    return 1;
+                }
+            }
+        }
+        $this->memo[$cur][$k] = ($cur == count($stones) - 1) ? 1 : 0;
+        return $this->memo[$cur][$k];
+    }
 }
 
 mock();
@@ -33,5 +61,8 @@ function mock()
     echo "======= test case start =======\n";
     var_dump((new Solution())->canCross([0, 1, 3, 5, 6, 8, 12, 17])) . "\n";
     var_dump((new Solution())->canCross([0, 1, 2, 3, 4, 8, 9, 11])) . "\n";
+
+    var_dump((new Solution())->canCross_2([0, 1, 3, 5, 6, 8, 12, 17])) . "\n";
+    var_dump((new Solution())->canCross_2([0, 1, 2, 3, 4, 8, 9, 11])) . "\n";
     echo "======= test case end =======\n";
 }
