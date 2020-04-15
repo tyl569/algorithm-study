b124301fc145ba7fcd4aa1bb24a0872d7037c45a
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-15 13:56:34 +0800

    增加LeetCode题目

--- LeetCode/647. 回文子串[中等].php
+++ LeetCode/647. 回文子串[中等].php
@@ -23,6 +23,42 @@ class Solution
         }
         return $ans;
     }
+
+    function countSubstrings_2($s)
+    {
+        $len = strlen($s);
+        $rebuild[0] = "@";
+        $rebuild[1] = "#";
+
+        $i = 2;
+        $arr = str_split($s);
+        // 组装成 @#a#b#c#$
+        foreach ($arr as $char) {
+            $rebuild[$i++] = $char;
+            $rebuild[$i++] = "#";
+        }
+        $rebuild[2 * $len + 2] = "$";
+        $center = 0;
+        $right = 0;
+        $max = [];
+        for ($i = 1; $i < count($rebuild); $i++) {
+            if ($i < $right) {
+                $max[$i] = min($right - $i, $max[2 * $center - $i]);
+            }
+            while ($rebuild[$i + $max[$i] + 1] == $rebuild[$i - $max[$i] - 1]) {
+                $max[$i]++;
+            }
+            if ($i + $max[$i] > $right) {
+                $center = $i;
+                $right = $i + $max[$i];
+            }
+        }
+        $count = 0;
+        foreach ($max as $m) {
+            $count += intval(($m + 1) / 2);
+        }
+        return $count;
+    }
 }
 
 mock();
@@ -31,6 +67,7 @@ function mock()
 {
     echo "======= test case start =======\n";
     echo (new Solution())->countSubstrings("aaa") . "\n";
-    echo (new Solution())->countSubstrings("abc") . "\n";
+    echo (new Solution())->countSubstrings_2("abc") . "\n";
+    echo (new Solution())->countSubstrings_2("aaa") . "\n";
     echo "======= test case end =======\n";
 }
