6a7f74a22128386bb33c3d20854e9042a4b7e99e
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-16 16:25:32 +0800

    增加dp方法

--- LeetCode/213. 打家劫舍 II[中等].php
+++ LeetCode/213. 打家劫舍 II[中等].php
@@ -33,6 +33,34 @@ class Solution
         );
         return $res;
     }
+
+    function rob_2($nums)
+    {
+        if (empty($nums)) {
+            return 0;
+        }
+        if (count($nums) == 1) {
+            return $nums[0];
+        }
+
+        return max(
+            $this->dp(1, $nums, count($nums) - 1), // 从第二个房子开始抢，一直到最后一间房子
+            $this->dp(0, $nums, count($nums) - 2) // 从第一间房子开始抢，一直到倒数第二间房子
+        );
+    }
+
+    function dp($start, $nums, $end)
+    {
+        $dp_i = 0;
+        $dp_i_1 = 0;
+        $dp_i_2 = 0;
+        for ($i = $start; $i <= $end; $i++) {
+            $dp_i = max($dp_i_1, $dp_i_2 + $nums[$i]);
+            $dp_i_2 = $dp_i_1;
+            $dp_i_1 = $dp_i;
+        }
+        return $dp_i;
+    }
 }
 
 
@@ -43,5 +71,8 @@ function mock()
     echo "======= test case start =======\n";
     echo (new Solution())->rob([2, 3, 2]) . "\n";
     echo (new Solution())->rob([1, 2, 3, 1]) . "\n";
+
+    echo (new Solution())->rob_2([2, 3, 2]) . "\n";
+    echo (new Solution())->rob_2([1, 2, 3, 1]) . "\n";
     echo "======= test case end =======\n";
 }
