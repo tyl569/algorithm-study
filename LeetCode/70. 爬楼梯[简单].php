bc4ee0487048dd0985fe597a42130e0625285f92
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-18 17:20:01 +0800

    修改代码

--- LeetCode/70. 爬楼梯[简单].php
+++ LeetCode/70. 爬楼梯[简单].php
@@ -9,53 +9,58 @@ class Solution
      */
     function climbStairs($n)
     {
-        if ($n == 1 || $n == 2) {
-            return $n;
+        if ($n == 0) {
+            return 0;
+        }
+        if ($n == 1) {
+            return 1;
+        }
+        if ($n == 2) {
+            return 2;
+        }
+        return $this->climbStairs($n - 1) + $this->climbStairs($n - 2);
+    }
+
+    private $memo = [];
+
+    function climbStairs_2($n)
+    {
+        if ($n == 0) {
+            return 0;
+        }
+        if ($n == 1) {
+            return 1;
+        }
+        if ($n == 2) {
+            return 2;
+        }
+        $this->memo[0] = 0;
+        $this->memo[1] = 1;
+        $this->memo[2] = 2;
+        if (!empty($this->memo[$n])) {
+            return $this->memo[$n];
+        }
+        $this->memo[$n] = $this->climbStairs($n - 1) + $this->climbStairs($n - 2);
+        return $this->memo[$n];
+    }
+
+    function climbStairs_3($n)
+    {
+
+        if ($n == 1) {
+            return 1;
+        }
+        if ($n == 2) {
+            return 2;
         }
         $num1 = 1;
         $num2 = 2;
         for ($i = 3; $i <= $n; $i++) {
             $tmp = $num2;
-            $num2 = $num2 + $num1;
+            $num2 += $num1;
             $num1 = $tmp;
         }
         return $num2;
-    }
-
-    function climbStairs_2($n)
-    {
-        $base = [
-            [1, 1],
-            [1, 0]
-        ];
-        $ans = [
-            [1, 0],
-            [0, 1]
-        ];
-        while ($n) {
-            if ($n & 1) {
-                $ans = $this->multi($ans, $base);
-            }
-            $base = $this->multi($base, $base);
-            $n >>= 1;
-        }
-        return $ans[0][0];
-    }
-
-    function multi($a, $b)
-    {
-        $c = [
-            [0, 0],
-            [0, 0]
-        ];
-        for ($i = 0; $i < 2; $i++) {
-            for ($j = 0; $j < 2; $j++) {
-                for ($k = 0; $k < 2; $k++) {
-                    $c[$i][$j] = $c[$i][$j] + $a[$i][$k] * $b[$k][$j];
-                }
-            }
-        }
-        return $c;
     }
 }
 
@@ -65,7 +70,8 @@ mock();
 function mock()
 {
     echo "======= test case start =======\n";
-    echo (new Solution())->climbStairs(3) . "\n";
-    echo (new Solution())->climbStairs_2(3) . "\n";
+    echo (new Solution())->climbStairs(30) . "\n";
+    echo (new Solution())->climbStairs_2(30) . "\n";
+    echo (new Solution())->climbStairs_3(30) . "\n";
     echo "======= test case end =======\n";
 }
