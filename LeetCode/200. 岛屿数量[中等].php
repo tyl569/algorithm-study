e0ca82734319bbc1aa877d630038b494e42edff0
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-18 13:22:56 +0800

    增加并查集解法

--- LeetCode/200. 岛屿数量[中等].php
+++ LeetCode/200. 岛屿数量[中等].php
@@ -72,6 +72,114 @@ class Solution
         return $count;
 
     }
+
+    function numIslands_3($grid)
+    {
+        if (empty($grid)) {
+            return 0;
+        }
+        $rows = count($grid);
+        $cols = count($grid[0]);
+        $uf = new UnionFind($grid);
+        $step = [
+            [0, -1],
+            [-1, 0],
+            [1, 0],
+            [0, 1]
+        ];
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $cols; $j++) {
+                if ($grid[$i][$j] == 1) {
+                    for ($k = 0; $k < count($step); $k++) {
+                        $newI = $i + $step[$k][0];
+                        $newJ = $j + $step[$k][1];
+                        if (isset($grid[$newI][$newJ]) && $grid[$newI][$newJ] == 1) {
+                            $uf->union($this->node($i, $j, $cols), $this->node($newI, $newJ, $cols));
+                        }
+                    }
+                }
+            }
+        }
+        return $uf->getCount();
+    }
+
+    function node($i, $j, $cols)
+    {
+        return $i * $cols + $j;
+    }
+}
+
+class UnionFind
+{
+    private $count = 0;
+
+    /**
+     * @return mixed
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    private $size;
+    private $parent = [];
+
+    public function __construct($grid)
+    {
+        for ($i = 0; $i < count($grid); $i++) {
+            for ($j = 0; $j < count($grid[0]); $j++) {
+                if ($grid[$i][$j] == 1) {
+                    $this->count++;
+                    $this->parent[$i * count($grid[0]) + $j] = $i * count($grid[0]) + $j;
+                    $this->size[$i * count($grid[0]) + $j] = $i * count($grid[0]) + $j;
+                }
+            }
+        }
+    }
+
+    // 返回根节点
+    public function find($x)
+    {
+        while ($this->parent[$x] != $x) {
+            // 节点压缩
+            $this->parent[$x] = $this->parent[$this->parent[$x]];
+            $x = $this->parent[$x];
+        }
+        return $x;
+    }
+
+    public function union($a, $b)
+    {
+        $rootA = $this->find($a);
+        $rootB = $this->find($b);
+        if ($rootB == $rootA) {
+            return;
+        }
+        if ($rootB >= $rootA) {
+            $this->parent[$rootA] = $rootB;
+            $this->size[$rootB] += $this->size[$rootA];
+        } else {
+            $this->parent[$rootB] = $rootA;
+            $this->size[$rootA] += $this->size[$rootB];
+        }
+        $this->count--;
+    }
+
+    // 验证根节点是否是同一个
+    public function connected($a, $b)
+    {
+        $rootA = $this->find($a);
+        $rootB = $this->find($b);
+        return $rootA == $rootB;
+    }
 }
 
 mock();
@@ -87,5 +195,16 @@ function mock()
     ];
     echo (new Solution())->numIslands($grid) . "\n";
     echo (new Solution())->numIslands_2($grid) . "\n";
+    echo (new Solution())->numIslands_3($grid) . "\n";
+
+    $grid = [
+        [1, 1, 1, 1, 0],
+        [1, 1, 0, 1, 0],
+        [1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+    echo (new Solution())->numIslands($grid) . "\n";
+    echo (new Solution())->numIslands_2($grid) . "\n";
+    echo (new Solution())->numIslands_3($grid) . "\n";
     echo "======= test case end =======\n";
 }
