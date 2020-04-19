49ad5a17b9a243668a34ef83817a2199d979ff5b
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-19 22:48:00 +0800

    增加普通bfs的解法

--- LeetCode/127. 单词接龙[中等].php
+++ LeetCode/127. 单词接龙[中等].php
@@ -10,16 +10,16 @@ class Solution
      * @return Integer
      */
 
-
+    // 普通BFS
     function ladderLength($beginWord, $endWord, $wordList)
     {
         if (!in_array($endWord, $wordList)) {
             return 0;
         }
         $wordList = array_flip($wordList);
-        $s1[] = $beginWord;
-        $s2[] = $endWord;
-        $n = strlen($beginWord);
+        $s1[] = $beginWord; // 开始的单词
+        $s2[] = $endWord; // 结束的单词
+        $n = strlen($beginWord); // 单词长度
         $level = 0;
         while (!empty($s1)) {
             $level++;
@@ -30,11 +30,12 @@ class Solution
             }
             $s = [];
             foreach ($s1 as $word) {
-                for ($i = 0; $i < $n; $i++) {
+                for ($i = 0; $i < $n; $i++) { // 挨个准备替换单词中的字母
                     $word1 = $word;
+                    // 循环a-z的字母
                     for ($ch = ord('a'); $ch <= ord('z'); $ch++) {
                         $word1[$i] = chr($ch);
-                        if (in_array($word1, $s2)) {
+                        if (in_array($word1, $s2)) { // 如果替换后的单词在目标数组里面，个数+1
                             return ++$level;
                         }
                         if (!array_key_exists($word1, $wordList)) {
@@ -49,6 +50,56 @@ class Solution
         }
         return 0;
     }
+
+    function ladderLength_2($beginWord, $endWord, $wordList)
+    {
+        if (!in_array($endWord, $wordList)) {
+            return 0;
+        }
+        $queue = new SplQueue();
+        $visited = [];
+        $queue->push($beginWord);
+        $count = 0;
+        while (!$queue->isEmpty()) {
+            $size = $queue->count();
+            $count++;
+            for ($i = 0; $i < $size; $i++) {
+                $str = $queue->dequeue();
+                foreach ($wordList as $word) {
+                    if (in_array($word, $visited)) {
+                        continue;
+                    }
+                    if (!$this->canConvert($str, $word)) {
+                        continue;
+                    }
+                    if ($word == $endWord) { // 如果找到了目标字符串
+                        return $count + 1;
+                    }
+                    $visited[] = $word;
+                    $queue->push($word);
+                }
+            }
+        }
+        return 0;
+    }
+
+    function canConvert($str1, $str2)
+    {
+        if (strlen($str1) != strlen($str2)) {
+            return false;
+        }
+        $count = 0;
+        for ($i = 0; $i < strlen($str1); $i++) {
+            if ($str1{$i} != $str2{$i}) {
+                $count++;
+                if ($count > 1) {
+                    return false;
+                }
+            }
+        }
+        return $count == 1;
+    }
+
 }
 
 mock();
@@ -58,10 +109,15 @@ function mock()
     echo "======= test case start =======\n";
     var_dump((new Solution())->ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]));
     var_dump((new Solution())->ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log"]));
-    var_dump((new Solution())->ladderLength("hit", "dot", ["hot", "dot", "dog", "lot", "log"]));
     var_dump((new Solution())->ladderLength("hit", "log", ["hot", "dot", "dog", "lot", "log"]));
     var_dump((new Solution())->ladderLength("hot", "dog", ["hot", "dog"]));
+    var_dump((new Solution())->ladderLength("lost", "miss", ["most", "mist", "miss", "lost", "fist", "fish"]));
 
+    var_dump((new Solution())->ladderLength_2("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]));
+    var_dump((new Solution())->ladderLength_2("hit", "cog", ["hot", "dot", "dog", "lot", "log"]));
+    var_dump((new Solution())->ladderLength_2("hit", "log", ["hot", "dot", "dog", "lot", "log"]));
+    var_dump((new Solution())->ladderLength_2("hot", "dog", ["hot", "dog"]));
+    var_dump((new Solution())->ladderLength_2("lost", "miss", ["most", "mist", "miss", "lost", "fist", "fish"]));
 
     echo "======= test case end =======\n";
 }
