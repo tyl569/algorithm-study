435c6de8f8aacf0a605ed46b2fc1317345761408
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-23 22:40:19 +0800

    修改代码

--- LeetCode/1. 两数之和[简单].php
+++ LeetCode/1. 两数之和[简单].php
@@ -12,11 +12,12 @@ class Solution
     {
         $arr = [];
         for ($i = 0; $i < count($nums); $i++) {
-            $diff = $target - $nums[$i];
-            $arr[$diff] = $i;
             if (isset($arr[$nums[$i]])) {
                 return [$i, $arr[$nums[$i]]];
             }
+            $diff = $target - $nums[$i];
+            $arr[$diff] = $i;
+
         }
         return [];
     }
@@ -26,4 +27,7 @@ $ret = (new Solution())->twoSum([2, 7, 11, 15], 9);
 var_dump($ret);
 
 $ret = (new Solution())->twoSum([1, 4, 5, 7], 9);
+var_dump($ret);
+
+$ret = (new Solution())->twoSum([3, 2, 4], 6);
 var_dump($ret);
