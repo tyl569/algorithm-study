694f80a05ac7fc3a2f974cd770cdbf5d3d4dedf4
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-04-25 08:48:43 +0800

    修改代码

--- LeetCode/51. N皇后[困难].php
+++ LeetCode/51. N皇后[困难].php
@@ -14,22 +14,26 @@ class Solution
         $default = "";
         for ($i = 0; $i < $n; $i++) {
             $default .= ".";
+
         }
-        $matrix = array_fill(0, $n, $default);
+        for ($i = 0; $i < $n; $i++) {
+            $matrix[] = $default;
+        }
         $this->helper($matrix, $n, 0);
         return $this->result;
+
     }
 
     function helper(&$matrix, $n, $row)
     {
-        if ($row == $n) {
+        if ($n == $row) {
             $this->result[] = $matrix;
             return;
         }
-        for ($j = 0; $j < $n; $j++) {
+
+        for ($j = 0; $j < strlen($matrix[$row]); $j++) {
             if (!$this->invalid($matrix, $row, $j)) {
                 continue;
-
             }
             $matrix[$row]{$j} = "Q";
             $this->helper($matrix, $n, $row + 1);
@@ -40,27 +44,29 @@ class Solution
 
     function invalid($matrix, $row, $col)
     {
-        // 检查当前这一列是否有皇后
-        for ($i = 0; $i < count($matrix); $i++) {
+        for ($i = 0; $i < $col; $i++) { // 检测当前列有没有皇后
+            if ($matrix[$row]{$i} == "Q") {
+                return false;
+            }
+        }
+        for ($i = 0; $i < $row; $i++) { // 检测当前行有没有皇后
             if ($matrix[$i]{$col} == "Q") {
                 return false;
             }
         }
-
         $i = $row - 1;
         $j = $col - 1;
-        // 向左上角查看有没有Q
         while ($i >= 0 && $j >= 0) {
-            if ($matrix[$i]{$j} == "Q") {
+            if ($matrix[$i]{$j} == "Q") { // 如果左上角有Q，返回false
                 return false;
             }
             $i--;
             $j--;
         }
+
         $i = $row - 1;
         $j = $col + 1;
-        // 向右上角查看有没有Q
-        while ($i >= 0 && $j <= strlen($matrix[0]) - 1) {
+        while ($i >= 0 && $j <= strlen($matrix[$row]) - 1) { // 如果右上角有Q，返回false
             if ($matrix[$i]{$j} == "Q") {
                 return false;
             }
@@ -68,6 +74,7 @@ class Solution
             $j++;
         }
         return true;
+
 
     }
 }
