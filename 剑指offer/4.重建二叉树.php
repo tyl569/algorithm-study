d66cb6aec47bee119ec0991c474c1084863e608a
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-05-04 22:11:47 +0800

    修改代码

--- 剑指offer/4.重建二叉树.php
+++ 剑指offer/4.重建二叉树.php
@@ -14,6 +14,7 @@ require_once "../Struct/TreeNode.php";
 
 function reConstructBinaryTree($pre, $vin)
 {
+    $vin = array_flip($vin);
     $root = helper($pre, 0, count($pre) - 1, $vin, 0, count($vin) - 1);
     return $root;
 }
@@ -24,11 +25,48 @@ function helper($pre, $preStart, $preEnd, $in, $inStart, $inEnd)
         return null;
     }
     $root = new TreeNode($pre[$preStart]);
-    for ($i = $inStart; $i <= $inEnd; $i++) {
-        if ($in[$i] == $pre[$preStart]) {
-            $root->left = helper($pre, $preStart + 1, $preStart + $i - $inStart, $in, $inStart, $i - 1);
-            $root->right = helper($pre, $preStart + $i - $inStart + 1, $preEnd, $in, $i + 1, $inEnd);
-        }
-    }
+    $i = $in[$pre[$preStart]];
+    $root->left = helper($pre, $preStart + 1, $preStart + $i - $inStart, $in, $inStart, $i - 1);
+    $root->right = helper($pre, $preStart + $i - $inStart + 1, $preEnd, $in, $i + 1, $inEnd);
     return $root;
 }
+
+//$rootIndex = 0;
+
+//
+//function reConstructBinaryTree_2($pre, $vin)
+//{
+//    $vin = array_flip($vin);
+//    $root = helper_2(0, count($pre), $pre, $vin);
+//    return $root;
+//}
+//
+//function helper_2($start, $end, $pre, $vin)
+//{
+//    global $rootIndex;
+//    if ($start == $end || $rootIndex >= count($pre)) {
+//        return null;
+//    }
+//
+//    $rootVal = $pre[$rootIndex];
+//    $root = new TreeNode($rootVal);
+//    $rootIndexOfVin = $vin[$rootVal];
+//    $rootIndex++;
+//    $root->left = helper_2($start, $rootIndexOfVin, $pre, $vin);
+//    $root->right = helper_2($rootIndexOfVin + 1, $end, $pre, $vin);
+//    return $root;
+//}
+
+
+//$pre = [1, 2, 4, 7, 3, 5, 6, 8];
+//$vin = [4, 7, 2, 1, 5, 3, 8, 6];
+
+
+$pre = [1, 2, 4, 3, 5, 6];
+$vin = [4, 2, 1, 5, 3, 6];
+//$root = reConstructBinaryTree($pre, $vin);
+//TreeNode::printTree($root);
+//echo "\n";
+$root = reConstructBinaryTree($pre, $vin);
+TreeNode::printTree($root);
+
