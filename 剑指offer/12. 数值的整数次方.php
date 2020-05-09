060ba83ac209ff2f332d3809a18c1a08c9e45cca
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-05-09 22:46:40 +0800

    增加题目

---
+++ 剑指offer/12. 数值的整数次方.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @param $base
+ * @param $exponent
+ *
+ *
+ * 题目描述
+ * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ *
+ * 保证base和exponent不同时为0
+ */
+function Power($base, $exponent)
+{
+    if ($exponent < 0) {
+        $base = 1 / $base;
+    }
+    $cur = $base;
+    $ret = 1;
+    for ($i = $exponent; $i; $i = intval($i / 2)) {
+        if ($i & 1) {
+            $ret *= $cur;
+        }
+        $cur *= $cur;
+    }
+    return $ret;
+}
+
+echo Power(2, 3) . "\n";
