fe55c9fe36df63d8db725ccdf4f9360508c07c6a
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-05-10 17:02:22 +0800

    增加迭代解法

--- 剑指offer/16. 合并两个排序的链表.php
+++ 剑指offer/16. 合并两个排序的链表.php
@@ -25,4 +25,30 @@ function Merge($pHead1, $pHead2)
     }
 }
 
+function Merge_2($pHead1, $pHead2)
+{
+    $node = new ListNode(null);
+    $ret = $node;
+    while ($pHead1 && $pHead2) {
+        if ($pHead1->val < $pHead2->val) {
+            $node->next = $pHead1;
+            $pHead1 = $pHead1->next;
+        } else {
+            $node->next = $pHead2;
+            $pHead2 = $pHead2->next;
+        }
+        $node = $node->next;
+
+    }
+
+    if ($pHead1 != null) {
+        $node->next = $pHead1;
+    }
+
+    if ($pHead2 != null) {
+        $node->next = $pHead2;
+    }
+    return $ret->next;
+}
+
 
