0ff4dc7e85bc4299c1f3a7f2dd940aa1d200bfd1
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-05-22 23:39:13 +0800

    修改代码

--- LeetCode/2. 两数相加[两数相加].php
+++ LeetCode/2. 两数相加[两数相加].php
@@ -21,27 +21,49 @@ class Solution
     function addTwoNumbers($l1, $l2)
     {
         $head = new ListNode(0);
-        $p = $l1;
-        $q = $l2;
         $cur = $head;
-        $curry = 0;
-        while ($p != null || $q != null) {
-            $x = is_null($p) ? 0 : $p->val;
-            $y = is_null($q) ? 0 : $q->val;
-            $sum = $x + $y + $curry;
-            $curry = (int)($sum / 10);
-            $cur->next = new ListNode($sum % 10);
+
+        $tmp = 0; // 用来统计进制
+        while ($l1 != null || $l2 != null) {
+            $x = is_null($l1) ? 0 : $l1->val;
+            $y = is_null($l2) ? 0 : $l2->val;
+            $curVal = $x + $y + $tmp;
+            $cur->next = new ListNode($curVal % 10);
             $cur = $cur->next;
-            if ($p != null) {
-                $p = $p->next;
+            $tmp = intval($curVal / 10);
+            if ($l1 != null) {
+                $l1 = $l1->next;
             }
-            if ($q != null) {
-                $q = $q->next;
+            if ($l2 != null) {
+                $l2 = $l2->next;
             }
         }
-        if ($curry > 0) {
-            $cur->next = new ListNode($curry);
+        if ($tmp > 0) {
+            $cur->next = new ListNode($tmp);
         }
         return $head->next;
     }
+}
+
+mock();
+
+function mock()
+{
+    echo "======= test case start =======\n";
+    $l11 = new ListNode(2);
+    $l12 = new ListNode(4);
+    $l13 = new ListNode(3);
+    $l11->next = $l12;
+    $l12->next = $l13;
+
+
+    $l21 = new ListNode(5);
+    $l22 = new ListNode(6);
+    $l23 = new ListNode(4);
+    $l21->next = $l22;
+    $l22->next = $l23;
+
+    $ret = (new Solution())->addTwoNumbers($l11, $l21);
+    var_dump($ret);
+    echo "======= test case end =======\n";
 }
