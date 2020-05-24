d2eaa17e9ef60875a2ee16e232f0b1fd757b74ce
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-05-24 16:53:04 +0800

    增加备注

--- LeetCode/121. 买卖股票的最佳时机[简单].php
+++ LeetCode/121. 买卖股票的最佳时机[简单].php
@@ -33,8 +33,8 @@ class Solution
                 $dp[0][1] = -$prices[0];
                 continue;
             }
-            $dp[$i][0] = max($dp[$i - 1][0], $dp[$i - 1][1] + $prices[$i]);
-            $dp[$i][1] = max($dp[$i - 1][1],  - $prices[$i]);
+            $dp[$i][0] = max($dp[$i - 1][0], $dp[$i - 1][1] + $prices[$i]); // 第i天手上没有持有股票，说明前一天就没有股票，或者前一天购买了股票，但是今天卖掉了
+            $dp[$i][1] = max($dp[$i - 1][1], -$prices[$i]); // 第i天手上有股票，说明前一天手上就有股票，或者前一天没有股票，但是这次买入了股票
         }
         return $dp[count($prices) - 1][0];
     }
