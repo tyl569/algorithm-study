f302c221ebacd9a2a6a9b9fb4294a529783da592
tengyunlong <yunlongteng@sfmail.sf-express.com>
2020-06-27 17:49:24 +0800

    增加解法

--- LeetCode/560. 和为K的子数组[中等].php
+++ LeetCode/560. 和为K的子数组[中等].php
@@ -25,6 +25,28 @@ class Solution
         }
         return $count;
     }
+
+    function subarraySum_2($nums, $k)
+    {
+        $n = count($nums);
+        $ans = 0;
+        $hash = [];
+        $hash[0] = 1;
+        $preSum = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $preSum += $nums[$i];//前缀和
+            $diff = $preSum - $k;
+            if (isset($hash[$diff])) {
+                $ans += $hash[$diff];
+            }
+            if (isset($hash[$preSum])) {
+                $hash[$preSum]++;
+            } else {
+                $hash[$preSum] = 1;
+            }
+        }
+        return $ans;
+    }
 }
 
 mock();
@@ -32,9 +54,10 @@ mock();
 function mock()
 {
     echo "======= test case start =======\n";
-    echo (new Solution())->subarraySum([1, 1, 1], 2) . "\n";
-    echo (new Solution())->subarraySum([1, 2, 3], 3) . "\n";
-
+//    echo (new Solution())->subarraySum([1, 1, 1], 2) . "\n";
+//    echo (new Solution())->subarraySum([1, 2, 3], 3) . "\n";
+    echo (new Solution())->subarraySum_2([1, 1, 1], 2) . "\n";
+    echo (new Solution())->subarraySum_2([1, 2, 3], 3) . "\n";
 
     echo "======= test case end =======\n";
 }
